e73a3a6f45654d75bc3a347f8bb6e4257d7697b9
Tamer <tamerse21business@gmail.com>
2025-05-04 23:26:03 +0300

    Added Session Config...

---
+++ learn-bridge-back-end/src/main/java/com/learnbridge/learn_bridge_back_end/config/SessionConfig.java
@@ -0,0 +1,20 @@
+package com.learnbridge.learn_bridge_back_end.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("JSESSIONID");
+        serializer.setSameSite("None");
+        serializer.setUseSecureCookie(true);
+        serializer.setUseHttpOnlyCookie(true);
+        return serializer;
+    }
+}
