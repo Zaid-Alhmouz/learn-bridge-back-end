9660806b48eeaf4709182c5b99e4498df3734ab5
Zaid <blackdice2121@gmail.com>
2025-05-16 20:16:49 +0300

    fixed some issues with reports

--- learn-bridge-back-end/src/main/java/com/learnbridge/learn_bridge_back_end/dao/ReportDAOImpl.java
+++ learn-bridge-back-end/src/main/java/com/learnbridge/learn_bridge_back_end/dao/ReportDAOImpl.java
@@ -4,7 +4,6 @@ import com.learnbridge.learn_bridge_back_end.entity.Report;
 import com.learnbridge.learn_bridge_back_end.entity.ReportStatus;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,67 +14,60 @@ import java.util.List;
 public class ReportDAOImpl implements ReportDAO {
 
     @PersistenceContext
-    EntityManager entityManager;
-
+    private EntityManager entityManager;
 
     @Override
     @Transactional
     public void saveReport(Report report) {
-
         entityManager.persist(report);
     }
 
     @Override
     @Transactional
     public void updateReport(Report report) {
-
         entityManager.merge(report);
     }
 
     @Override
     @Transactional
     public void deleteReport(Long reportId) {
-
         Report report = entityManager.find(Report.class, reportId);
         entityManager.remove(entityManager.contains(report) ? report : entityManager.merge(report));
     }
 
     @Override
     public Report findReportByReportId(Long reportId) {
-
         return entityManager.find(Report.class, reportId);
     }
 
     @Override
     public List<Report> findReportsBySessionId(Long sessionId) {
-        String sqlStatement = "select r from Report r where r.session.sessionId = :sessionId";
-        TypedQuery<Report> query = entityManager.createQuery(sqlStatement, Report.class);
+        String jpql = "select r from Report r where r.session.sessionId = :sessionId";
+        TypedQuery<Report> query = entityManager.createQuery(jpql, Report.class);
         query.setParameter("sessionId", sessionId);
         return query.getResultList();
     }
 
     @Override
     public List<Report> findAllReports() {
-        return entityManager.createQuery("select r from Report r", Report.class).getResultList();
+        String jpql = "select r from Report r";
+        TypedQuery<Report> query = entityManager.createQuery(jpql, Report.class);
+        return query.getResultList();
     }
 
     @Override
     public List<Report> findPendingReports() {
-        String sqlStatement = "select r from Report r where r.reportedUser = :status";
-        TypedQuery<Report> query = entityManager.createQuery(sqlStatement, Report.class);
+        String jpql = "select r from Report r where r.reportStatus = :status";
+        TypedQuery<Report> query = entityManager.createQuery(jpql, Report.class);
         query.setParameter("status", ReportStatus.PENDING);
-
         return query.getResultList();
-
     }
 
     @Override
     public List<Report> findResolvedReports() {
-        String sqlStatement = "select r from Report r where r.reportStatus = :status";
-        TypedQuery<Report> query = entityManager.createQuery(sqlStatement, Report.class);
+        String jpql = "select r from Report r where r.reportStatus = :status";
+        TypedQuery<Report> query = entityManager.createQuery(jpql, Report.class);
         query.setParameter("status", ReportStatus.RESOLVED);
-
-
         return query.getResultList();
     }
 }
